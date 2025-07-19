73d00882ed36ff49b1e22ebcddd6e68d7b5c6687
Abid Azad <149910776+abid3a@users.noreply.github.com>
2025-07-19 13:06:32 -0400

    Updated connections.tsx

--- app/(tabs)/connections.tsx
+++ app/(tabs)/connections.tsx
@@ -160,7 +160,7 @@ export default function ConnectionsScreen() {
   );
 
   return (
-    <SafeAreaView style={[styles.container, { backgroundColor: '#fff' }]}> 
+    <SafeAreaView style={styles.container}> 
       <StatusBar style="dark" backgroundColor="#fff" />
       {/* Fixed header, filter, and search */}
       {renderHeader()}
